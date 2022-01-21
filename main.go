60039bf7ed75dd68d7fa7753de2e91444e5726a2
Yurino Oga <yurino.o@latona.io>
2022-01-21 12:38:33 +0900

    release

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-process-order-confirmation-reads/SAP_API_Caller"
+	"sap-api-integrations-process-order-confirmation-reads/sap_api_input_reader"
+
+	"github.com/latonaio/golang-logging-library/logger"
+)
+
+func main() {
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs//SDC_Process_Order_Confirmation_Conf_By_OrderID_Conf_Group_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
+	)
+
+	accepter := inoutSDC.Accepter
+	if len(accepter) == 0 || accepter[0] == "All" {
+
+		accepter = []string{
+			"ConfByOrderID", "MaterialMovements",
+			"ConfByOrderIDConfGroup",
+		}
+	}
+
+	caller.AsyncGetProcessOrderConfirmation(
+		inoutSDC.ProcessOrderConfirmation.OrderID,
+		inoutSDC.ProcessOrderConfirmation.MaterialMovements.Batch,
+		inoutSDC.ProcessOrderConfirmation.ConfirmationGroup,
+		accepter,
+	)
+}
